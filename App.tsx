6b8fed6e8483b68239b762f48e22c71100bda4c9
Isaac Hussain <isaaclearns58@gmail.com>
2024-01-18 13:56:33 +0300

    chore: add react-native-paper to the project

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
-import {Provider} from 'react-redux';
-import {store} from './src/services/state/store';
 
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <SafeAreaView className="flex-1 bg-black">
-        <HomeScreen />
-      </SafeAreaView>
-    </Provider>
+    <SafeAreaView className="flex-1 bg-black">
+      <HomeScreen />
+    </SafeAreaView>
   );
 }
 
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 /**
  * @format
  */
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import {store} from './src/services/state/store';
+import {MD3LightTheme as DefaultTheme, PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+const Main = () => {
+  return (
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <App />
+      </PaperProvider>
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
